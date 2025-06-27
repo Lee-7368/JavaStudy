b52931df6d601b31ca2943cdfa3fa4a91dcf34ab
Lee-7368 <alpagod8@gmail.com>
2025-06-27 16:57:36 +0900

    4회차 임시저장

--- src/Begin.java
+++ src/Begin.java
@@ -3,8 +3,13 @@ import java.io.IOException;
 public class Begin {
     public static void main(String[] args) throws IOException {
 
+        System.out.print("숫자 입력 : ");
 
+        //콘솔에 숫자를 입력하면 입력한 내용이 공간에 저장하도록됨
+        int num = System.in.read();// 입력받은 값을 아스키 코드 값으로 저장
 
-        }
-
+        System.out.println(num-48);
     }
+}
+
+//  랜덤으로 훌수 짝수 게임 만들기
