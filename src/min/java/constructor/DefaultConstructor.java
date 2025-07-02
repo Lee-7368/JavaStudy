8f6f01fbbd0622d633198f2da2a382b06b6c0b7d
Lee-7368 <alpagod8@gmail.com>
2025-07-02 18:35:31 +0900

    6회차 업로드/오버라이딩

---
+++ src/min/java/constructor/DefaultConstructor.java
@@ -0,0 +1,10 @@
+package min.java.constructor;
+
+public class DefaultConstructor {
+    public DefaultConstructor() {
+
+        System.out.println("기본생성자를 호출한다");
+    }
+}
+// 자바프로젝트 새로 만들어서 페키지 만들어서
+//산술연자
---
+++ src/min/java/constructor/DefaultConstructorCall.java
@@ -0,0 +1,8 @@
+package min.java.constructor;
+
+public class DefaultConstructorCall {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        DefaultConstructorCall defaultConstructorCall = new DefaultConstructorCall();
+    }
+}
